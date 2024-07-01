846c60c8fe6f243f40c9333e597ece010d4fe427
xingchong <xingchong6@163.com>
2024-07-01 13:43:26 +0800

    [tianye]tianye server is running successfully.

---
+++ m78-all/Tianye/Tianye-service/src/test/java/run/mone/local/docean/dubbo/TestAdaptive.java
@@ -0,0 +1,15 @@
+package run.mone.local.docean.dubbo;
+
+import org.junit.jupiter.api.Test;
+
+public class TestAdaptive {
+
+    @Test
+    public void testAdaptive() {
+        Protocol$Adaptive protocol$Adaptive = new Protocol$Adaptive();
+        protocol$Adaptive.destroy();
+    }
+
+}
+
+
